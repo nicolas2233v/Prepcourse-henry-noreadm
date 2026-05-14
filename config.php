1f9aeba73b6355168c018b11454b2378d704d4ca
Nicolas Villegas <94490069+nicolas2233v@users.noreply.github.com>
2026-05-14 02:24:09 -0300

    Modernize spiritual landing page

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$SITE_NAME = 'Consultorio Espiritual';
+$WHATSAPP_NUMBER = getenv('WHATSAPP_NUMBER') ?: '5491123456789';
+$GOOGLE_ADS_ID = getenv('GOOGLE_ADS_ID') ?: 'AW-16645573761';
+$GTM_ID = getenv('GTM_ID') ?: 'GTM-KHPMSF9W';
